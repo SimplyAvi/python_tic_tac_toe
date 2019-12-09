915ac30c4db1fe60ed5fab660ff16483cf6c3280
SimplyAvi <a.totaram@gmail.com>
2019-12-09 11:11:03 -0500

    adding the game logic for the checks and user turns

--- index.py
+++ index.py
@@ -106,3 +106,67 @@ def player_choice(board):
 def replay():
 
     return input('Do you want to play again? Enter Yes or No: ').lower().startswith('y')
+# asking the players if they want to play another game
+
+
+
+
+# game logic with the users turns
+print('Welcome to Tic Tac Toe!')
+
+while True:
+    #     Set the game
+    the_board = [' '] * 10
+    player1_marker, player2_marker = player_input()
+    turn = choose_first()
+    print(turn + ' will go first')
+
+    play_game = input('Are you ready to play? Enter Yes or No')
+
+    if play_game.lower()[0] == 'y':
+        game_on = True
+    else:
+        game_on = False
+
+    while game_on:
+
+        if turn == 'Player 1':
+            #Player 1 Turn
+            display_board(the_board)
+            position = player_choice(the_board)
+            place_marker(the_board, player1_marker, position)
+
+            if win_check(the_board, player1_marker):
+                display_board(the_board)
+                print('Congratulations! Player 1 has won!')
+                game_on = False
+
+            else:
+                if full_board_check(the_board):
+                    display_board(the_board)
+                    print('This game is a draw')
+                    break
+                else:
+                    turn = 'Player 2'
+
+        else:
+            #Player2's Turn.
+            display_board(the_board)
+            position = player_choice(the_board)
+            place_marker(the_board, player2_marker, position)
+
+            if win_check(the_board, player2_marker):
+                display_board(the_board)
+                print('Congratulations!, Player 2 has won!')
+                game_on = False
+            else:
+                if full_board_check(the_board):
+                    display_board(the_board)
+                    print('This game is a draw')
+                    break
+                else:
+                    turn = 'Player 1'
+
+    if not replay():
+        break
+# game - user logic to exicute the game
